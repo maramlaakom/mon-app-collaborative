f8d795ce3bbf15e0d7ba7a4306f68a5c89e8b8d7
sarra088 <sarramlika8@gmail.com>
2026-02-09 16:17:36 +0100

    fix: Résolution des conflits sur le message de bienvenue

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
-print('Bienvenue sur notre application collective Bon travail à l\'équipe')
+print('Bienvenue sur notre App Collaborative Hello World de l\'équipe ')
 print('Fonctionnalité de login ajoutée par Etudiant A.')
 print('Fonctionnalité de tableau de bord ajoutée par Etudiant B.')
